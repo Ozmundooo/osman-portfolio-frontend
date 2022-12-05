49d9edc11e2a493810f9e973b2c67736e9f63ca7
Ozmundooo <osman.ali.dev@gmail.com>
2022-12-05 18:21:45 -0500

    added work page

--- pages/work.js
+++ pages/work.js
@@ -1,5 +1,30 @@
 import React from "react";
+import Header from "../components/Header";
+import WorksList from "../components/WorksList";
+import { sanityClient, urlFor } from "../lib/sanity/client";
+import { worksQuery } from "../lib/sanity/worksQuery";
+import ContactForm from "../components/ContactForm";
 
-export default function work() {
-  return <div>work</div>;
+export default function Home({ works }) {
+  return (
+    <>
+      <WorksList works={works} />
+      <ContactForm />
+    </>
+  );
 }
+export const getServerSideProps = async () => {
+  const works = await sanityClient.fetch(worksQuery);
+  if (!works.length && !testimonials.length) {
+    return {
+      props: {
+        works: [],
+      },
+    };
+  } else
+    return {
+      props: {
+        works,
+      },
+    };
+};
